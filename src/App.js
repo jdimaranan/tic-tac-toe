c1e8fe55767341464891f71bef50fa1879f72fb3
Carlo Dimaranan <85856061+jdimaranan@users.noreply.github.com>
2023-02-23 16:02:27 +0800

    add leaderboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import xamLogo from "./assets/xam-o.png"
 import easterEgg from "./assets/ester-egg.jpeg"
+import axios from 'axios';
+
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
@@ -10,10 +12,21 @@ function App() {
   const [playerNameOne, setPlayerNameOne] = useState("");
   const [playerNameTwo, setPlayerNameTwo] = useState("");
   const [error, setHasError] = useState("");
+  const [leaderboard, setLeaderboard] = useState({});
 
   const inputOneRef = useRef(null);
   const inputTwoRef = useRef(null);
 
+  useEffect(() => {
+    const fetchLeaderboard = async () => {
+      await axios.get("https://emnjerry-api-xam-technight.azurewebsites.net/api/Score").then(res => {
+        setLeaderboard(res.data);
+      }) 
+    }
+    
+    fetchLeaderboard();
+    }, [])
+;
   const renderImage = (letter) => {
     if (letter === "X") return <img src={xamLogo} style={{ width: 40, height: 40 }} />;
     else if (letter === "O") return <img src={easterEgg} style={{ width: 40, height: 40 }} />;
@@ -25,10 +38,17 @@ function App() {
     const player2 = inputTwoRef.current.value;
 
     if (player1 !== player2) {
-      setPlayerNameOne(player1);
-      setPlayerNameTwo(player2);
       setHasError(false);
     } else setHasError(true);
+
+    uploadName(player1, player2);
+  }
+
+  const uploadName = async (name1, name2) => {
+    await axios.put(`https://emnjerry-api-xam-technight.azurewebsites.net/api/Score/players/${name1}`).then((res) => 
+    setPlayerNameOne(name1));
+
+    await axios.put(`https://emnjerry-api-xam-technight.azurewebsites.net/api/Score/players/${name2}`).then((res) => setPlayerNameTwo(name2));
   }
 
   function handleClick(index) {
@@ -54,25 +74,30 @@ function App() {
   function renderBoard() {
     return (
       <div style={{ textAlign: "center"}}>
-      <h2>Xam Tic-Tac-Toe</h2>
-      <p>Player 1: <span style={{ fontWeight: "bold" }}>{playerNameOne}</span></p>
-      <p>Player 2: <span style={{ fontWeight: "bold" }}>{playerNameTwo}</span></p>
-      <div className="status" style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: 10 }}><span>{winner ? "Winner: " : "Next Move: "}</span>{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+        <h2>Xam Tic-Tac-Toe</h2>
+        <p>Player 1: <span style={{ fontWeight: "bold" }}>{playerNameOne}</span></p>
+        <p>Player 2: <span style={{ fontWeight: "bold" }}>{playerNameTwo}</span></p>
+        <div className="status" style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: 10 }}><span>{winner ? "Winner: " : "Next Move: "}</span>{status}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+
+        <p>Leaderboard</p>
+        {leaderboard.map((val, index) => {
+          return <div key={index}><p>Name:{val.name} Score: {val.score}</p></div>
+        })}
     </div>
     )
   }
@@ -105,29 +130,33 @@ function App() {
   return ( 
     <>
       { hasNames ? renderBoard() : renderPlayersForm() }
+
+      <br />
     </>
   );
-}
 
-function calculateWinner(squares) {
-  const lines = [    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+  function calculateWinner(squares) {
+    const lines = [    [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
     }
-  }
 
-  return null;
+  
+    return null;
+  }
 }
+
 
 export default App;
